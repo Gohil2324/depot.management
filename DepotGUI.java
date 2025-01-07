3ab4a762500f5bbab8e25643cadcb5a481a27e8c
Gohil2324 <167921815+Gohil2324@users.noreply.github.com>
2025-01-07 12:31:38 +0000

    Update DepotGUI.java

--- DepotGUI.java
+++ DepotGUI.java
@@ -2,7 +2,7 @@ package com.depot.management;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+  import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DepotGUI {
